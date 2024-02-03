576b6fa1541078eafccd272a0ea5f2c32ba0b599
Lucas Nascimento <nascimento.ti@gmail.com>
2024-02-03 08:28:29 -0300

    feat: create invoice on booking

--- model/invoice.py
+++ model/invoice.py
@@ -6,21 +6,26 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
+
 from database import db
 
 
-class Booking(db.Model):
-    __tablename__ = "booking"
+class Invoice(db.Model):
+    __tablename__ = "invoice"
 
     id: Mapped[str] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    itinerary_id: Mapped[Optional[str]] = mapped_column(ForeignKey("itinerary.id"))
+    booking_id: Mapped[Optional[str]] = mapped_column(ForeignKey("booking.id"))
 
-    payer_name: Mapped[str] = mapped_column(db.String, nullable=False)
-    payer_email: Mapped[str] = mapped_column(db.String, nullable=False)
-    payer_phone: Mapped[str] = mapped_column(db.String, nullable=False)
-    payer_document: Mapped[str] = mapped_column(db.String, nullable=False)
+    invoice_id: Mapped[Optional[str]] = mapped_column(db.String, nullable=False)
+
+    status: Mapped[str] = mapped_column(db.String, nullable=False)
+    method: Mapped[str] = mapped_column(db.String, nullable=False)
+    due_date: Mapped[str] = mapped_column(db.DateTime, nullable=False)
+    total_cents: Mapped[int] = mapped_column(db.Integer, nullable=False, server_default="0")
+
+    invoice_url: Mapped[Optional[str]] = mapped_column(db.String, nullable=True)
 
     is_deleted: Mapped[bool] = mapped_column(db.Boolean, default=False)
     inserted_at: Mapped[str] = mapped_column(
--- routes/public/booking.py
+++ routes/public/booking.py
@@ -1,11 +1,17 @@
+import json
+
+import iugu
+import requests
 from flask import Blueprint, request
 from marshmallow import Schema, ValidationError, fields
 
+from config import IUGU_API_TOKEN
+from database import db
 from model.booking import Booking
 from model.bookingtraveler import BookingTraveler
 from model.institution import Institution
+from model.invoice import Invoice
 from model.itinerary import Itinerary
-from database import db
 from routes.responses import create_error_response, create_response
 
 booking_bp = Blueprint("booking", __name__)
@@ -22,6 +28,8 @@ class BookingSchema(Schema):
     payer_name = fields.Str(required=True)
     payer_email = fields.Email(required=True)
     payer_phone = fields.Str(required=True)
+    payer_document = fields.Str(required=True)
+    payment_method = fields.Str(required=True)
     travelers = fields.List(fields.Nested(TravelerSchema), required=True)
 
 
@@ -58,6 +66,7 @@ def create_reservation(itinerary_id):
         payer_name=booking["payer_name"],
         payer_email=booking["payer_email"],
         payer_phone=booking["payer_phone"],
+        payer_document=booking["payer_document"],
     )
     db.session.add(new_booking)
     db.session.flush()
@@ -72,6 +81,65 @@ def create_reservation(itinerary_id):
         )
         db.session.add(new_traveler)
 
+    data = {
+        "email": booking["payer_email"],
+        "due_date": itinerary.purchase_deadline.strftime("%Y-%m-%d"),
+        "items": [
+            {
+                "description": traveler["traveler_name"],
+                "quantity": 1,
+                "price_cents": get_cents(itinerary.seat_price),
+            }
+            for traveler in booking["travelers"]
+        ],
+        "payer": {
+            "name": booking["payer_name"],
+            "email": booking["payer_email"],
+            "phone": booking["payer_phone"],
+            "cpf_cnpj": booking["payer_document"],
+        },
+        "payable_with": [booking["payment_method"]],
+        "max_installments_value": itinerary.installments,
+    }
+
+    total_cents = sum([get_cents(itinerary.seat_price) for _ in booking["travelers"]])
+    pix_discount_cents = int(total_cents * itinerary.pix_discount)
+
+    if booking["payment_method"] == "pix":
+        data["discount_cents"] = pix_discount_cents
+
+    url = f"https://api.iugu.com/v1/invoices?api_token={IUGU_API_TOKEN}"
+
+    payload = json.dumps(data)
+    headers = {
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    invoice_iugu = response.json()
+
+    new_invoice = Invoice(
+        booking_id=new_booking.id,
+        invoice_id=invoice_iugu["id"],
+        status=invoice_iugu["status"],
+        method=booking["payment_method"],
+        due_date=invoice_iugu["due_date"],
+        invoice_url=invoice_iugu["secure_url"],
+        total_cents=total_cents,
+    )
+    db.session.add(new_invoice)
+
+    # update booking with invoice id
+    new_booking.invoice_id = invoice_iugu["id"]
+    db.session.add(new_booking)
+
     db.session.commit()
 
-    return create_response({"booking_id": new_booking.id})
+    return create_response(
+        {"booking_id": new_booking.id, "invoice_url": invoice_iugu["secure_url"]}
+    )
+
+
+def get_cents(value: float) -> int:
+    return int(value * 100)
